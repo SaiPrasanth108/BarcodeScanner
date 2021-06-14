bd5f2640ce94afca393a1f0912f421d8bc1ce115
saiprasanthr <er.saiprasanthr@gmail.com>
2021-06-14 16:16:22 +0530

    Barcode scanner plugin for ios completed

---
+++ src/ios/BarCodeScanner.swift
@@ -0,0 +1,29 @@
+
+@objc(BarCodeScanner) 
+class BarCodeScanner : CDVPlugin{
+
+    var barCode: String?
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    
+    @objc(scanBarcode:) 
+    func scanBarcode(_ command: CDVInvokedUrlCommand) {
+        
+        let vc = ScannerViewController()
+        vc.modalPresentationStyle = .overCurrentContext
+        vc.modalTransitionStyle = .crossDissolve
+        vc.delegate = self
+        self.viewController.present(vc, animated: false, completion: nil)
+        
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Done")
+        
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
+    
+}
+
+extension BarCodeScanner: ScannerViewControllerDelegate {
+    func found(code: String) {
+        barCode = code
+        print("Barcode : ", barCode ?? "Not get barcode")
+    }
+}
--- www/BarCodeScanner.js
+++ www/BarCodeScanner.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'BarCodeScanner', 'coolMethod', [arg0]);
+  exec(success, error, 'BarCodeScanner', 'coolMethod', [arg0]);
+};
+
+exports.scanBarcode = function (arg0, success, error) {
+  exec(success, error, 'BarCodeScanner', 'scanBarcode', [arg0]);
 };
